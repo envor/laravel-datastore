f300829dd59be1e545e2c778510209885e723bd7
= <inman.turbo@gmail.com>
2024-02-14 01:02:19 -0500

    work in progress

--- src/HasDatastores.php
+++ src/HasDatastores.php
@@ -2,8 +2,14 @@
 
 namespace Envor\Datastore;
 
+use Envor\Platform\Concerns\HasPlatformUuids;
+use Envor\Platform\Concerns\UsesPlatformConnection;
+
 trait HasDatastores
 {
+    use HasPlatformUuids;
+    use UsesPlatformConnection;
+
     public function datastores()
     {
         return $this->morphMany(Datastore::class, 'owner');
