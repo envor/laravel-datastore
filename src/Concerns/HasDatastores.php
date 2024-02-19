e6787f2b58397ba47e1be5078b7fafe159da8196
= <inman.turbo@gmail.com>
2024-02-19 17:42:35 -0500

    coding styles

--- src/Concerns/HasDatastores.php
+++ src/Concerns/HasDatastores.php
@@ -3,10 +3,11 @@
 namespace Envor\Datastore\Concerns;
 
 use Envor\Datastore\Models\Datastore;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait HasDatastores
 {
-    public function datastores()
+    public function datastores() : MorphMany
     {
         return $this->morphMany(Datastore::class, 'owner');
     }
