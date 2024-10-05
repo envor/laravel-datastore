096a38bf8588af482307c5153f77b4ab1b615625
inmanturbo <47095624+inmanturbo@users.noreply.github.com>
2024-10-05 10:09:45 -0400

    Update datastore.php

--- config/datastore.php
+++ config/datastore.php
@@ -4,4 +4,5 @@
 return [
     'model' => \Envor\Datastore\Models\Datastore::class,
     'create_databases' => env('DATASTORE_CREATE_DATABASES', true),
+    'push_middleware => env('DATASTORE_PUSH_CONTEXT_MIDDLEWARE', false),
 ];
