5261fcf7afa0fefc6bda77946d554a1c61f5b1e9
inmanturbo <inmanturbo@users.noreply.github.com>
2024-10-05 14:51:45 +0000

    Fix styling

--- rector.php
+++ rector.php
@@ -6,11 +6,11 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/config',
-        __DIR__ . '/resources',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-        __DIR__ . '/workbench',
+        __DIR__.'/config',
+        __DIR__.'/resources',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+        __DIR__.'/workbench',
     ])
     // uncomment to reach your current PHP version
     // ->withPhpSets()
